62d5e68b0d4d9f23c41cd481a647966157d356c4
dsnjturtle <sebastian.t.kirchner@gmail.com>
2025-12-14 14:05:11 +0100

    feat: q2

--- src/everybody_codes/main.py
+++ src/everybody_codes/main.py
@@ -1,65 +1,78 @@
 import logging
 
+import numpy as np
 from ecd import get_inputs, submit
+from matplotlib import pyplot as plt
 
 logger = logging.getLogger("everybody_codes")
 
 
+def _divide(a: complex, b: complex) -> complex:
+    _a = int(a.real / b.real)
+    _b = int(a.imag / b.imag)
+    return complex(_a, _b)
+
+
 def part1(input_string: str) -> str:
-    parts = input_string.split("\n\n")
-    names, instructions = parts[0].split(","), parts[1].split(",")
+    parts = eval(input_string.replace("A=", ""))
+    a = complex(parts[0], parts[1])
 
-    pos = 0
-    ll = len(names)
-    while instructions:
-        i = instructions.pop(0)
-        d, n = 1 if i.startswith("R") else -1, int(i[1:])
-        n = d * n
-        pos += n
-        pos = min(max(pos, 0), ll - 1)
+    res = complex(0, 0)
+    for _ in range(3):
+        res *= res
+        res = _divide(res, complex(10, 10))
+        res += a
 
-    return names[pos]
+    return str([int(res.real), int(res.imag)]).replace(" ", "")
+
+
+def compute(input_string: str, step: int, n_steps: int) -> str:
+    parts = eval(input_string.replace("A=", ""))
+    a = complex(parts[0], parts[1])
+
+    grid = []
+    n_engraved = 0
+    for i in range(n_steps):
+        row = []
+        for j in range(n_steps):
+            pos = a + step * j * 1j + step * i
+
+            res = complex(0, 0)
+            stopped = False
+            for _ in range(100):
+                res *= res
+                res = _divide(res, complex(100_000, 100_000))
+                res += pos
+
+                if abs(res.real) > 1000000 or abs(res.imag) > 1000000:
+                    stopped = True
+                    row.append(0)
+                    break
+
+            if not stopped:
+                n_engraved += 1
+                row.append(1)
+
+        grid.append(row)
+
+    if True:
+        _g = np.array(grid, dtype=int) * 256
+        plt.imshow(_g, cmap="gray", interpolation="nearest")
+        plt.show()
+
+    return str(n_engraved)
 
 
 def part2(input_string: str) -> str:
-    parts = input_string.split("\n\n")
-    names, instructions = parts[0].split(","), parts[1].split(",")
-
-    pos = 0
-    ll = len(names)
-    while instructions:
-        i = instructions.pop(0)
-        d, n = 1 if i.startswith("R") else -1, int(i[1:])
-        n = d * n
-        pos += n
-        pos = pos % ll
-
-    return names[pos]
+    return compute(input_string, 10, 101)
 
 
 def part3(input_string: str) -> str:
-    parts = input_string.split("\n\n")
-    names, instructions = parts[0].split(","), parts[1].split(",")
-
-    pos = 0
-    ll = len(names)
-    while instructions:
-        i = instructions.pop(0)
-        d, n = 1 if i.startswith("R") else -1, int(i[1:])
-        n = d * n
-        pos += n
-        pos = pos % ll
-
-        old_name = names[0]
-        names[0] = names[pos]
-        names[pos] = old_name
-        pos = 0
-
-    return names[pos]
+    return compute(input_string, 1, 1001)
 
 
 if __name__ == "__main__":
-    quest = 1
+    quest = 2
     event = 2025
     data = get_inputs(quest=quest, event=event)
 
